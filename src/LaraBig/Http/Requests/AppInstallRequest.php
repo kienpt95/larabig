ea58c4064b65958029c49abf513bb739aebb17f8
longkt <longkt@smartosc.com>
2020-02-21 17:37:58 +0700

    update code

--- src/LaraBig/Http/Requests/AppInstallRequest.php
+++ src/LaraBig/Http/Requests/AppInstallRequest.php
@@ -7,4 +7,34 @@ use Illuminate\Foundation\Http\FormRequest;
 class AppInstallRequest extends FormRequest
 {
     //todo validate request
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorrize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'code'=>'required',
+            'scope'=>'required',
+            'context'=>'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'Not enough information was passed to install this app.'
+        ];
+    }
 }
