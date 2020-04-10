235d17f250dbaee37d5bb5359711193feac549ed
Kien Pham <kienpt2@smartosc.com>
2020-04-10 15:45:21 +0700

    Add constain for order status

--- src/LaraBig/Contracts/ApiModel/OrderStatus.php
+++ src/LaraBig/Contracts/ApiModel/OrderStatus.php
@@ -14,6 +14,22 @@ interface OrderStatus
     const PREFIX = "order_statuses";
     const VERSION = "";
 
+    const INCOMPLETE = 0;
+    const PENDING = 1;
+    const SHIPPED = 2;
+    const PARTIALLY_SHIPPED = 3;
+    const REFUNDED = 4;
+    const CANCELLED = 5;
+    const DECLINED = 6;
+    const AWAITING_PAYMENT = 7;
+    const AWAITING_PICKUP = 8;
+    const AWAITING_SHIPMENT = 9;
+    const COMPLETED = 10;
+    const AWAITING_FULFILLMENT = 11;
+    const MANUAL_VERIFY_REQUIRED = 12;
+    const DISPUTED = 13;
+    const PARTIAL_REFUNDED = 14;
+
     /**
      * Returns a Collection of All Order Statuses.
      * GET /order_statuses
